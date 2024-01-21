e7b828b8daab030832aa5d0c02f9ffe1f911a505
Bett <bettpatrick080@gmail.com>
2024-01-21 20:18:59 +0300

    fetch basing on category

--- index.js
+++ index.js
@@ -1,49 +1,67 @@
 document.addEventListener("DOMContentLoaded",() => {
     let url =""
     let category = document.querySelector("#category")
-    category.addEventListener("change",(e)=>{
+    category.addEventListener("click",(e)=>{
         // console.log(e.target.value)
         if(e.target.value ==="programming") {
-            url = "https://v2.jokeapi.dev/joke/Programming?amount=5"
+            document.querySelector(".jokes-container").innerHTML = ""
+            url = "https://v2.jokeapi.dev/joke/Programming?amount=10"
             fetchJokes(url)
         }
         else if(e.target.value ==="misc") {
-            url = "https://v2.jokeapi.dev/joke/Misc"
+            document.querySelector(".jokes-container").innerHTML = ""
+            url = "https://v2.jokeapi.dev/joke/Misc?amount=10"
             fetchJokes(url)
         }
         else if(e.target.value ==="pun") {
-            url = "https://v2.jokeapi.dev/joke/Pun"
+            document.querySelector(".jokes-container").innerHTML = ""
+            url = "https://v2.jokeapi.dev/joke/Pun?amount=10"
             fetchJokes(url)
         }
         else if(e.target.value ==="spooky") {
-            url = "https://v2.jokeapi.dev/joke/Spooky"
+            document.querySelector(".jokes-container").innerHTML = ""
+            url = "https://v2.jokeapi.dev/joke/Spooky?amount=10"
             fetchJokes(url)
         }
         else if(e.target.value ==="christmas") {
-            url = "https://v2.jokeapi.dev/joke/Christmas"
+            document.querySelector(".jokes-container").innerHTML = ""
+            url = "https://v2.jokeapi.dev/joke/Christmas?amount=10"
             fetchJokes(url)
         }
         
     })
-
+    //display any jokes when no category is chosen
+    fetchJokes()
     //fetch jokes based on category
-    function fetchJokes(url){
+    function fetchJokes(url = "https://v2.jokeapi.dev/joke/Any?amount=10"){
         fetch(url)
         .then(res => res.json())
-        .then((jokes) => {
-            console.log(jokes)
+        .then((jokesObject) => {
+            let jokes = jokesObject.jokes
+            // console.log(jokes)
             jokes.forEach((joke) => {
-                console.log(joke.setup)
-                renderJokes(joke.setup)        
+                // console.log(joke)
+                renderJokes(joke)        
             })
         })
     }
     //display the jokes based on category
     function renderJokes(joke){
-        console.log(joke)
+        // console.log(joke.setup)
         let jokeDiv = document.createElement("div")
+        if (joke.type === 'single') {
+            console.log(joke)
+            console.log(joke.joke)
+            jokeDiv.textContent = joke.joke
+        }else if (joke.type === 'twopart') {
+            console.log(joke)
+            let setup = joke.setup
+            let delivery = joke.delivery
+            console.log(`${setup}\n${delivery}`)
+            jokeDiv.textContent = `${setup}\n${delivery}`
+        }
         jokeDiv.id = "joke-div"
-        jokeDiv.textContent = joke
+        jokeDiv.textContent = joke.setup
         document.querySelector(".jokes-container").appendChild(jokeDiv)
     }
 })
